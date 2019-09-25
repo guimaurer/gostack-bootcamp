b5ceb98b7c65bc9ac92ce9fec0f2c35bcc3d4bd4
guimaurer <guilhermemaurer@hotmail.com>
2019-09-25 15:54:43 -0300

    termino primeiro modulo gobarber

--- modulo002/src/app/controllers/SessionController.js
+++ modulo002/src/app/controllers/SessionController.js
@@ -1,20 +1,35 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 
 import User from '../models/User';
+
 import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ Error: 'Validation Fails' });
+    }
+
     const { email, password } = req.body;
+
     const user = await User.findOne({ where: { email } });
 
     if (!user) {
-      return res.status(401).json({ Error: 'User not found' });
+      return res.json(401).json({ error: 'Email does not match' });
     }
 
     if (!(await user.checkPassword(password))) {
-      return res.status(401).json({ error: 'Password does not math' });
+      return res.status(401).json({ error: 'Password does not match' });
     }
+
     const { id, name } = user;
 
     return res.json({
@@ -29,4 +44,5 @@ class SessionController {
     });
   }
 }
+
 export default new SessionController();
